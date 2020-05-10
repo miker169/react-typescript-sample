d439478071d7d1bbcfdf9454dcb87eb50598d136
Mike <mike@helens-imac.home>
2020-05-10 15:35:12 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom'
 
-function App() {
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './Todo.model'
+
+
+const App: React.FC = () =>{
+  const [todos, setTodos] = React.useState<Todo[]>([])
+  
+  
+  const todoAddHandler = (text: string) => {
+   setTodos(prevTodos => [
+     ...prevTodos,
+     {id: Math.random().toString(), text: text}
+     ]);
+  };
+  
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo onAddTodo={todoAddHandler}/>
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
     </div>
   );
 }
